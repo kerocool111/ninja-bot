7fce822e19255dd74f7d9a971e36df5b6db05539
kerocool111 <48702268+kerocool111@users.noreply.github.com>
2019-03-18 21:14:16 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NTQ1MDIwNzEyNTAzNzM4Mzgw.D26jtg.3gappgQYQTKZoJ-05rmM9N7-GH8';
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+clientInformation.login(process.env.BOT_TOKEN);
